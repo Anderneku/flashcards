634adbe0663c57c9d701bbf5da69d0d403d604b0
Anderneku <chukunekunwanwene@gmail.com>
2025-06-28 12:11:19 +0100

    pwa fixes... again

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,11 @@
   import * as Breadcrumb from "$lib/components/ui/breadcrumb";
 	setMode("dark");
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('Service worker registered', reg))
+    .catch(err => console.error('Service worker registration failed', err));
+}
 
 	
 </script>
