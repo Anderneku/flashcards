53c55101fb6cc5bbc4e0185a56212ed3fc2668f7
Anderneku <chukunekunwanwene@gmail.com>
2025-06-28 11:43:59 +0100

    pwa things

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit(), SvelteKitPWA({
+      strategies: 'generateSW',
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png'],
+      manifest: {
+        name: 'Fancy Cards',
+        short_name: 'Cards',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/favicon.png',
+            sizes: '128x128',
+            type: 'image/png'
+          }
+        ]
+      }
+    })]
 });
